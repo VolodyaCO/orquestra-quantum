95005eae81be3dd2de980387aac30ecd605ec4ad
Salonik Resch <salonik.resch@zapatacomputing.com>
2022-06-23 18:35:32 -0500

    Adding distribution metrics and tests

---
+++ src/orquestra/quantum/distributions/moment_based_distance.py
@@ -0,0 +1,58 @@
+import math
+from typing import TYPE_CHECKING, Dict, Optional
+
+if TYPE_CHECKING:
+    from orquestra.quantum.distributions import MeasurementOutcomeDistribution
+
+
+def compute_moment_based_distance(
+    target_distribution: "MeasurementOutcomeDistribution",
+    measured_distribution: "MeasurementOutcomeDistribution",
+    M: Optional[int] = 15
+) -> float:
+    """Compute the moment based distance between two distributions,
+    potentially a target distribution and a measured distribution.
+
+    Args:
+        target_distribution: The target probability distribution.
+        measured_distribution: The measured probability distribution.
+        M: The number of iterations to reach convergence
+
+    Returns:
+        The value of the moment based distance
+    """
+
+    #The starting distance is 0
+    distance = 0.0 
+    #Get all bitstrings of first distribution
+    target_keys = target_distribution.distribution_dict.keys() 
+    #Get all  bitstrings of second distribution
+    measured_keys = measured_distribution.distribution_dict.keys() 
+    #Combine all bitstrings together
+    all_keys = set(target_keys).union(measured_keys) 
+
+    #Gamma parameter
+    n_qubits = len(next(iter(all_keys)))
+    gamma = 2**n_qubits
+
+    #Go sufficiently many iterations to get accuracy
+    for m in range(M):
+        s = 0
+        for bitstring in all_keys:
+            x = int("".join([str(b) for b in bitstring]),2)
+            scaler = (x/gamma)**m
+
+            target_bitstring_value = target_distribution.distribution_dict.get(
+                bitstring,0
+            )
+            measured_bitstring_value = measured_distribution.distribution_dict.get(
+                bitstring,0
+            )
+
+            diff = target_bitstring_value - measured_bitstring_value
+
+            s += abs(scaler*diff)
+
+        distance += 1/math.factorial(m) * s
+
+    return distance/2 
---
+++ src/orquestra/quantum/distributions/total_variation_distance.py
@@ -0,0 +1,44 @@
+import math
+from typing import TYPE_CHECKING, Dict
+
+if TYPE_CHECKING:
+    from orquestra.quantum.distributions import MeasurementOutcomeDistribution
+
+
+def compute_total_variation_distance(
+    target_distribution: "MeasurementOutcomeDistribution",
+    measured_distribution: "MeasurementOutcomeDistribution"
+) -> float:
+    """Compute the total variation distance between two distributions,
+    potentially a target distribution and a measured distribution.
+
+    Args:
+        target_distribution: The target probability distribution.
+        measured_distribution: The measured probability distribution.
+
+    Returns:
+        The value of the the total variation distance
+    """
+
+    #The starting value is 0
+    value = 0.0 
+    #Get all bitstrings of first distribution
+    target_keys = target_distribution.distribution_dict.keys() 
+    #Get all  bitstrings of second distribution
+    measured_keys = measured_distribution.distribution_dict.keys() 
+    #Combine all bitstrings together
+    all_keys = set(target_keys).union(measured_keys) 
+
+    for bitstring in all_keys:
+        #Get the probability of each bitstring for each distribution (if missing, it is 0)
+        target_bitstring_value = target_distribution.distribution_dict.get(
+            bitstring,0
+        )
+        measured_bitstring_value = measured_distribution.distribution_dict.get(
+            bitstring,0
+        )
+
+        #Sum of abs of all probability differences
+        value += abs(target_bitstring_value - measured_bitstring_value)
+
+    return value/2
